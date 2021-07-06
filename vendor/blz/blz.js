9bae46d9b24aea98a48f2ba1c2376f04d0c9799e
Blazed Development Group <tyler.ruff@blazed.work>
2021-07-06 18:13:34 +0000

    separated standard library

---
+++ vendor/blz/blz.js
@@ -0,0 +1,73 @@
+/* 
+    blz/main.js
+    The Blazed Stardard JS Library
+*/
+
+/* Toggle visibility */
+
+function mx(item){
+    var i = document.getElementById(item);
+    if(i.style.display == 'block'){
+      i.style.display = 'none';
+    } else {
+      i.style.display = 'block';
+    }
+  }
+  
+  
+  class BlazedNavElement extends 	HTMLElement {
+    constructor() {
+      // Always call super first in constructor
+      super();
+  
+      // Element functionality written in here
+  
+    }
+  }
+  
+  class BlazedHeroElement extends HTMLElement {
+    constructor() {
+      // Always call super first in constructor
+      super();
+  
+      // Element functionality written in here
+  
+    }
+  }
+  
+  class BlazedGridElement extends	HTMLElement {
+    constructor() {
+      // Always call super first in constructor
+      super();
+  
+      // Element functionality written in here
+  
+    }
+  }
+  
+  class BlazedFormElement extends HTMLFormElement {
+    constructor() {
+      // Always call super first in constructor
+      super();
+  
+      // Element functionality written in here
+  
+    }
+  }
+  
+  class BlazedFieldElement extends HTMLElement {
+    constructor() {
+      // Always call super first in constructor
+      super();
+  
+      // Element functionality written in here
+  
+    }
+  }
+  
+  
+  customElements.define('blz-nav', BlazedNavElement, { extends: 'nav' });
+  customElements.define('blz-grid', BlazedGridElement, { extends: 'div' });
+  customElements.define('blz-hero', BlazedHeroElement, { extends: 'div' });
+  customElements.define('blz-form', BlazedFormElement, { extends: 'form' });
+  customElements.define('blz-field',BlazedFieldElement, { extends: 'div' });
