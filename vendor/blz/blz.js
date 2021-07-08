ee5486a796def36fe7f61536baf86b4b72dbe6ea
Blazed Development Group <tyler.ruff@blazed.work>
2021-07-08 20:47:36 +0000

    updated blz-std

--- vendor/blz/blz.js
+++ vendor/blz/blz.js
@@ -5,69 +5,92 @@
 
 /* Toggle visibility */
 
-function mx(item){
-    var i = document.getElementById(item);
-    if(i.style.display == 'block'){
-      i.style.display = 'none';
-    } else {
-      i.style.display = 'block';
-    }
-  }
-  
-  
-  class BlazedNavElement extends 	HTMLElement {
-    constructor() {
-      // Always call super first in constructor
-      super();
-  
+function addBurger(){
+  var blz_nav = document.getElementsByTagName("BLZ-NAV")[0];
+  if(blz_nav !== undefined){
+      var menuId = blz_nav.getAttribute("menu");
+      var label = document.createElement("label");
+      label.setAttribute("id", "burger");
+      label.setAttribute("for", "nav-check");
+      label.addEventListener("mouseup", function(){
+        var i = document.getElementById(menuId);
+        if(i.style.display == 'block'){
+          i.style.display = 'none';
+        } else {
+          i.style.display = 'block';
+        }
+      });
       // Element functionality written in here
-  
-    }
+      var check = document.createElement("input");
+      check.setAttribute("type", "checkbox");
+      check.setAttribute("id", "nav-check");
+      var span_1 = document.createElement("span");
+      var span_2 = document.createElement("span");
+      var span_3 = document.createElement("span");
+    
+      label.appendChild(check);
+      label.appendChild(span_1);
+      label.appendChild(span_2);
+      label.appendChild(span_3);
+    
+      var mm = document.getElementById(menuId);
+      blz_nav.insertBefore(label, mm);
   }
-  
-  class BlazedHeroElement extends HTMLElement {
-    constructor() {
-      // Always call super first in constructor
-      super();
-  
-      // Element functionality written in here
-  
-    }
+}
+document.body.onload = addBurger;
+
+
+class BlazedNavElement extends 	HTMLElement {
+  constructor() {
+    // Always call super first in constructor
+    super();
+
   }
-  
-  class BlazedGridElement extends	HTMLElement {
-    constructor() {
-      // Always call super first in constructor
-      super();
-  
-      // Element functionality written in here
-  
-    }
+}
+
+class BlazedHeroElement extends HTMLElement {
+  constructor() {
+    // Always call super first in constructor
+    super();
+
+    // Element functionality written in here
+
   }
-  
-  class BlazedFormElement extends HTMLFormElement {
-    constructor() {
-      // Always call super first in constructor
-      super();
-  
-      // Element functionality written in here
-  
-    }
+}
+
+class BlazedGridElement extends	HTMLElement {
+  constructor() {
+    // Always call super first in constructor
+    super();
+
+    // Element functionality written in here
+
   }
-  
-  class BlazedFieldElement extends HTMLElement {
-    constructor() {
-      // Always call super first in constructor
-      super();
-  
-      // Element functionality written in here
-  
-    }
+}
+
+class BlazedFormElement extends HTMLFormElement {
+  constructor() {
+    // Always call super first in constructor
+    super();
+
+    // Element functionality written in here
+
   }
-  
-  
-  customElements.define('blz-nav', BlazedNavElement, { extends: 'nav' });
-  customElements.define('blz-grid', BlazedGridElement, { extends: 'div' });
-  customElements.define('blz-hero', BlazedHeroElement, { extends: 'div' });
-  customElements.define('blz-form', BlazedFormElement, { extends: 'form' });
-  customElements.define('blz-field',BlazedFieldElement, { extends: 'div' });
+}
+
+class BlazedFieldElement extends HTMLElement {
+  constructor() {
+    // Always call super first in constructor
+    super();
+
+    // Element functionality written in here
+
+  }
+}
+
+
+customElements.define('blz-nav', BlazedNavElement, { extends: 'nav' });
+customElements.define('blz-grid', BlazedGridElement, { extends: 'div' });
+customElements.define('blz-hero', BlazedHeroElement, { extends: 'div' });
+customElements.define('blz-form', BlazedFormElement, { extends: 'form' });
+customElements.define('blz-field',BlazedFieldElement, { extends: 'div' });
